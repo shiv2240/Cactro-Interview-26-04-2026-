78f8a93432cc7b0d4bff8bd2ed9fc7d8d9cbeab8
Shiv Sahni <shivsahni2240@gmail.com>
2026-04-26 12:29:53 +0530

    Fixed the graphQL version incompatibity for deployment over render

--- backend/index.js
+++ backend/index.js
@@ -3,7 +3,7 @@ const express = require('express');
 const cors = require('cors');
 const connectDB = require('./config/db');
 const releaseRoutes = require('./routes/releaseRoutes');
-const { graphqlHTTP } = require('express-graphql');
+const { createHandler } = require('graphql-http/lib/use/express');
 const { schema, root } = require('./graphql/schema');
 
 const app = express();
@@ -15,11 +15,10 @@ app.use(express.json());
 // Routes
 app.use('/api/releases', releaseRoutes);
 
-// GraphQL Route
-app.use('/graphql', graphqlHTTP({
+// Modern GraphQL Route
+app.all('/graphql', createHandler({
   schema: schema,
   rootValue: root,
-  graphiql: true, // Enables the GraphiQL UI
 }));
 
 app.listen(port, async () => {
